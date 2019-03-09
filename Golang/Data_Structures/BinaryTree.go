66888701dfc49b9f43135eccc66b5b34383cd977
LiuYikang <Lyiker@outlook.com>
2019-03-09 19:24:21 +0800

    add Binary Tree

---
+++ Golang/Data_Structures/BinaryTree.go
@@ -0,0 +1,87 @@
+package BinaryTree
+
+import (
+    "fmt"
+)
+
+type Node struct {
+	val   int
+	left  *Node
+	right *Node
+}
+
+type BinaryTree struct {
+    root *Node
+}
+
+func (tree *BinaryTree) Insert(val int) {
+    if tree.root == nil {
+        tree.root = &Node{val: val}
+        return
+    }
+
+    currentNode := tree.root
+    for {
+        if val > currentNode.val {
+            if currentNode.right == nil {
+                currnetNode.right = &Node{val: val}
+                return
+            }
+            currentNode = currentNode.right
+        } else {
+            if currentNode.left == nil {
+                currentNode.left = &Node{val: val}
+                return
+            }
+            currentNode = currentNode.left
+        }
+    }
+}
+
+func (tree *BinaryTree) Search(val int) (*Node, bool) {
+    if tree.root == nil {
+        return nil, false
+    }
+
+    currentNode := tree.root
+    for currentNode != nil {
+        if val == currentNode.val {
+            return currentNode, true
+        } else if (val < currentNode.val) {
+            currentNode = currentNode.left
+        } else {
+            currentNode = currentNode.right
+        }
+    }
+    return nil, false
+}
+
+func (tree *BinaryTree) InorderTraversal(subTree *BinaryTree) {
+    if subTree.left != nil {
+        tree.InorderTraversal(subTree.left)
+    }
+    fmt.Print(subTree.val)
+    if subTree.right != nil {
+        tree.InorderTraversal(subTree.right)
+    }
+}
+
+func (tree *BinaryTree) PreorderTraversal(subTree *BinaryTree) {
+    fmt.Print(subTree.val)
+    if (subTree.left != nil) {
+        tree.PreorderTraversal(subTree.left)
+    }
+    if (subTree.right != nil) {
+        tree.PreorderTraversal(subTree.right)
+    }
+}
+
+func (tree *BinaryTree) PostorderTraversal(subTree *BinaryTree) {
+    if (subTree.left != nil) {
+        tree.PostorderTraversal(subTree.left)
+    }
+    if (subTree.right != nil) {
+        tree.PostorderTraversal(subTree.right)
+    }
+    fmt.Print(subTree.val)
+}
